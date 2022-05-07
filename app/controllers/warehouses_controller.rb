ba692aab232b4656108ced0c1f984b5b6d9a7b9f
Maciel Júnior <macielfcjunior@icloud.com>
2022-05-07 15:30:32 -0300

    :sparkles: feat: Criando formulário para cadastrar galpões.

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -2,4 +2,24 @@ class WarehousesController < ApplicationController
   def show
     @warehouse = Warehouse.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    # Parameters: {"authenticity_token"=>"[FILTERED]", "warehouse"=>
+    # {"name"=>"Galpão RIo", "description"=>"Galpão no Rio de Janeiro", "code"=>"RIO", 
+    # "address"=>"Avenida do Museu do Amanhã", "city"=>"Rio de Janeiro", "cep"=>"72445-030", "area"=>"32000"}, 
+    # "commit"=>"Enviar"}
+
+    # 1 - Receber os dados enviados
+
+    # 2 - Criar um novo galpão no banco de dados
+    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area)
+    warehouse = Warehouse.new(warehouse_params)
+    warehouse.save()
+
+    # 3 - Redirecionar para a tela incial
+    redirect_to root_path
+  end
 end
