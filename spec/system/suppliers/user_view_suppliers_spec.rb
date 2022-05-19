e019218a43703bf10b98e7171b2900cc3e86e92b
Maciel Júnior <macielfcjunior@icloud.com>
2022-05-19 14:02:07 -0300

    :test_tube: test - Testes para verificar se os forncedores estão sendo exibidos corretamente

--- spec/system/suppliers/user_view_suppliers_spec.rb
+++ spec/system/suppliers/user_view_suppliers_spec.rb
@@ -6,9 +6,45 @@ describe 'Usuário vê fornecedores' do
 
     # Act 
     visit root_path
-    click_on 'Fornecedores'
+    within('nav') do
+      click_on 'Fornecedores'
+    end
+    
 
     # Assert
     expect(current_path).to eq suppliers_path
+  end
+
+  it 'com sucesso' do
+    # Arrage
+    Supplier.create!(corporate_name: 'ACME LTDA', brand_name: 'ACME', registration_number: '434472146', 
+                     full_address: 'Av das Palmeiras, 100', city: 'Bauru', state: 'SP', email: 'contato@acme.com')
+    Supplier.create!(corporate_name: 'Sparke industries Brazil LTDA', brand_name: 'Spark', registration_number: '434472147', 
+                     full_address: 'Torre da Indústria, 250', city: 'Teresina', state: 'PI', email: 'vendas@spark.com')
+
+    # Act 
+    visit root_path
+    click_on 'Fornecedores'
+
+    # Assert
+    expect(page).to have_content('Fornecedores')
+    expect(page).to have_content('ACME')
+    expect(page).to have_content('Bauru - SP')
+    expect(page).to have_content('Spark')
+    expect(page).to have_content('Teresina - PI')
+    expect(page).not_to have_content('Não existem fornecedores cadastrados')
+
+  end
+
+  it 'com sucesso' do
+    # Arrage
+    
+
+    # Act 
+    visit root_path
+    click_on 'Fornecedores'
+
+    # Assert
+    expect(page).to have_content('Não existem fornecedores cadastrados')
   end
 end
