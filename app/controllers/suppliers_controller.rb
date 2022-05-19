c86c04bfaee643baa48f3a305b6bcf7071d51973
Maciel Júnior <macielfcjunior@icloud.com>
2022-05-19 18:50:57 -0300

    :sparkles: feat - Função adicionar um fornecedor e testes que validam a sua criação

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -7,8 +7,28 @@ class SuppliersController < ApplicationController
 
   def show; end
 
+  def new
+    @supplier = Supplier.new
+  end
+
+  def create
+    @supplier = Supplier.new(supplier_params)
+
+    if @supplier.save()
+      flash[:notice] = "Fornecedor Cadastrado com Sucesso"
+      redirect_to suppliers_path
+    else
+      flash.now[:notice] = "Fornecedor não cadastrado." # utilizada apenas na requisição que ela é criada
+      render 'new'
+    end
+  end
+
   private
   def set_suppliers
     @supplier = Supplier.find(params[:id])
+  end
+
+  def supplier_params
+    params.require(:supplier).permit(:corporate_name, :brand_name, :registration_number, :full_address, :city, :state, :email)
   end
 end
---
+++ spec/system/suppliers/user_register_supplier_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+describe 'Usuário cadastra um fornecedor' do
+  it 'a partir do menu' do
+    # Arrage
+
+    # Act 
+    visit root_path
+    within('nav') do
+      click_on 'Fornecedores'
+    end
+    click_on 'Cadastrar novo fornecedor'
+    
+    # Assert
+    expect(page).to have_content('Nome Fantasia')
+    expect(page).to have_content('Razão Social')
+    expect(page).to have_content('CNPJ')
+    expect(page).to have_content('Endereço')
+    expect(page).to have_content('Cidade')
+    expect(page).to have_content('Estado')
+    expect(page).to have_content('E-mail')
+  end
+
+  it 'com sucesso' do
+    # Arrage
+
+    # Act 
+    visit root_path
+    within('nav') do
+      click_on 'Fornecedores'
+    end
+    click_on 'Cadastrar novo fornecedor'
+    fill_in 'Nome Fantasia', with: 'ACME'
+    fill_in 'Razão Social', with: 'ACME LTDA'
+    fill_in 'CNPJ', with: '47176140000189'
+    fill_in 'Endereço', with: 'Av das Palmas, 100'
+    fill_in 'Cidade', with: 'Bauru'
+    fill_in 'Estado', with: 'SP'
+    fill_in 'E-mail', with: 'contato@acme.com'
+    click_on 'Enviar'
+    
+    # Assert
+    expect(page).to have_content('Fornecedor Cadastrado com Sucesso')
+    expect(page).to have_content('ACME')
+    expect(page).to have_content('Bauru')
+    expect(page).to have_content('SP')
+  end
+
+  it 'com dados incompletos' do
+    # Arrage
+
+    # Act 
+    visit root_path
+    within('nav') do
+      click_on 'Fornecedores'
+    end
+    click_on 'Cadastrar novo fornecedor'
+    fill_in 'Nome Fantasia', with: ''
+    fill_in 'Razão Social', with: ''
+    fill_in 'CNPJ', with: '47176140000189'
+    fill_in 'Endereço', with: 'Av das Palmas, 100'
+    fill_in 'Cidade', with: ''
+    fill_in 'Estado', with: 'SP'
+    fill_in 'E-mail', with: 'contato@acme.com'
+    click_on 'Enviar'
+    
+    # Assert
+    expect(page).to have_content('Fornecedor não cadastrado')
+    expect(page).to have_content('Nome Fantasia não pode ficar em branco')
+    expect(page).to have_content('Razão Social não pode ficar em branco')
+    expect(page).to have_content('Cidade não pode ficar em branco')
+  end
+end
