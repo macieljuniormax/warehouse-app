cb5f6b97dbde82e14fbe6a42f3d57912cb1c5017
Maciel Júnior <macielfcjunior@icloud.com>
2022-05-23 23:09:43 -0300

    :sparkles: feat - incluindo a função de registrar um pedido (apenas para usuários autenticados)

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :warehouses, only: [:show, :new, :create, :edit, :update, :destroy]
   resources :suppliers, only: [:index, :show, :new, :create, :edit, :update]
   resources :product_models, only: [:index,:show, :new, :create]
+  resources :orders, only: [:new, :create, :show]
 
   # CRUD
   # Create  Criar
---
+++ spec/system/orders/user_register_order_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+describe 'Usuário cadastra um pedido' do
+  it 'e deve estar autenticado' do
+    # Arrange
+
+    # Act
+    visit root_path
+    click_on 'Registrar Pedido'
+
+    # Assert
+    expect(current_path).to eq new_user_session_path
+  end
+
+  it 'com sucesso' do
+    # Arrange
+    user = User.create!(name: 'Maciel', email: 'macieljunior@gmail.com', password: 'password')
+
+    Warehouse.create!(name: 'Belo Horizonte', code: 'BHZ', city: 'Belo Horizonte', area: 20_000, 
+      address: 'Avenida tiradentes, 20', cep: '46000-000', 
+      description: 'Galpão para cargas mineiras.')
+
+    warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000, 
+                                  address: 'Avenida do Aeroporto, 1000', cep: '15000-000', 
+                                  description: 'Galpão destinados para cargas internacionais.')
+    
+
+    supplier = Supplier.create!(corporate_name: 'Samsung Eletrônicos SA', brand_name: 'Samsung', 
+                                registration_number: '12.123.123/0001-00', 
+                                full_address: 'Av das Nações Unidas, 1000', city: 'São Paulo', 
+                                state: 'SP', email: 'sac@samsung.com.br')
+
+    
+
+    # Act
+    login_as(user)
+    visit root_path
+    click_on 'Registrar Pedido'
+    select warehouse.name, from: 'Galpão Destino'
+    select supplier.corporate_name, from: 'Fornecedor'
+    fill_in 'Data Prevista', with: '20/12/2022'
+    click_on 'Gravar'
+
+    # Assert
+    expect(page).to have_content 'Pedido registrado com sucesso.'
+    expect(page).to have_content 'Galpão de destino: Aeroporto SP'
+    expect(page).to have_content 'Fornecedor: Samsung Eletrônicos SA'
+    expect(page).to have_content 'Usuário responsável: Maciel | macieljunior@gmail.com'
+    expect(page).to have_content 'Data Prevista de Entrega: 20/12/2022'
+    expect(page).not_to have_content 'Belo Horizonte'
+    expect(page).not_to have_content 'Spark'
+  end
+end
