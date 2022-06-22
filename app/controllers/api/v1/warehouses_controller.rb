bcc9ec05bf630401ebed76cf71b55bf9e0e18694
Maciel Júnior <macielfcjunior@icloud.com>
2022-06-22 14:43:14 -0300

    feat: Cria API para ver todos galpões

--- app/controllers/api/v1/warehouses_controller.rb
+++ app/controllers/api/v1/warehouses_controller.rb
@@ -1,4 +1,14 @@
 class Api::V1::WarehousesController < ActionController::API
+  def index
+    begin
+      warehouses = Warehouse.all
+      render status: 200, json: warehouses.as_json(except: [:created_at, :updated_at])
+    rescue
+      render status: 404
+    end
+  end
+  
+  
   def show
     begin
       warehouse = Warehouse.find(params[:id])
@@ -6,6 +16,5 @@ class Api::V1::WarehousesController < ActionController::API
     rescue
       render status: 404
     end
-    
   end
 end
--- spec/requests/apis/warehouse_api_spec.rb
+++ spec/requests/apis/warehouse_api_spec.rb
@@ -27,8 +27,11 @@ describe "Warehouse API" do
 
   context "GET /api?v1?watrhouses" do
     it "sucess" do
-      warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000, 
+      warehouse_1 = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000, 
         address: 'Avenida do Aeroporto, 1000', cep: '15000-000', 
+        description: 'Galpão destinados para cargas internacionais.')
+      warehouse_2 = Warehouse.create!(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 100_000, 
+        address: 'Avenida do Aeroporto, 2000', cep: '45000-000', 
         description: 'Galpão destinados para cargas internacionais.')
 
       get "/api/v1/warehouses"
@@ -36,10 +39,24 @@ describe "Warehouse API" do
       expect(response.status).to eq 200
       expect(response.content_type).to include 'application/json'
       jason_responde = JSON.parse(response.body)
-      expect(jason_responde["name"]).to eq('Aeroporto SP')
-      expect(jason_responde["code"]).to eq('GRU')
-      expect(jason_responde.keys).not_to include('created_at')
-      expect(jason_responde.keys).not_to include('updated_at')    
+      expect(jason_responde.length).to eq 2
+      expect(jason_responde[0]["name"]).to eq 'Aeroporto SP'
+      expect(jason_responde[1]["name"]).to eq 'Rio'
+    end
+
+    it "return empty if there is no warehouse" do
+
+      get "/api/v1/warehouses"
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to include 'application/json'
+      jason_responde = JSON.parse(response.body)
+      expect(jason_responde).to eq []
     end
   end
+
+
+
+
+
 end
